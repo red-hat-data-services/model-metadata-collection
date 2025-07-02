4edab17721afd38c5d15fdcb47443ea18958cb5f
tarilabs <matteo.mortari@gmail.com>
2025-07-02 16:58:28 +0200

    replacing logs
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"bytes"
 	"compress/gzip"
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"strings"
@@ -31,16 +30,16 @@ func main() {
 
 func ScanLayersForModelCarD(layers []types.BlobInfo, src types.ImageSource) {
 	for i, layer := range layers {
-		fmt.Printf("Layer %d:\n", i+1)
-		fmt.Printf("  Digest: %s\n", layer.Digest)
-		fmt.Printf("  MediaType: %s\n", layer.MediaType)
-		fmt.Printf("  Size: %d bytes\n", layer.Size)
+		log.Printf("Layer %d:\n", i+1)
+		log.Printf("  Digest: %s\n", layer.Digest)
+		log.Printf("  MediaType: %s\n", layer.MediaType)
+		log.Printf("  Size: %d bytes\n", layer.Size)
 		if layer.Annotations != nil {
-			fmt.Printf("  Annotations: %v\n", layer.Annotations)
+			log.Printf("  Annotations: %v\n", layer.Annotations)
 
 			// Check if this layer has the modelcard annotation
 			if layerType, exists := layer.Annotations["io.opendatahub.modelcar.layer.type"]; exists && layerType == "modelcard" {
-				fmt.Printf("  Found modelcard layer! Attempting to access modelcard layer blob with digest: %s\n", layer.Digest)
+				log.Printf("  Found modelcard layer! Attempting to access modelcard layer blob with digest: %s\n", layer.Digest)
 
 				var layerBlob io.ReadCloser
 				var err error
@@ -57,11 +56,11 @@ func ScanLayersForModelCarD(layers []types.BlobInfo, src types.ImageSource) {
 				} else {
 					var reader io.Reader = layerBlob
 					defer layerBlob.Close()
-					fmt.Printf("  Successfully fetched modelcard layer blob. Attempting to read as tar...\n")
+					log.Printf("  Successfully fetched modelcard layer blob. Attempting to read as tar...\n")
 
 					// Check if it's a gzipped tar file
 					if strings.Contains(layer.MediaType, "+gzip") {
-						fmt.Printf("  Detected gzipped tar file, decompressing...\n")
+						log.Printf("  Detected gzipped tar file, decompressing...\n")
 						gzReader, err := gzip.NewReader(layerBlob)
 						if err != nil {
 							log.Printf("Error creating gzip reader: %v", err)
@@ -85,11 +84,11 @@ func ScanLayersForModelCarD(layers []types.BlobInfo, src types.ImageSource) {
 							log.Printf("Error reading tar: %v", err)
 							break
 						}
-						fmt.Printf("  Found file in tar: %s (size: %d bytes)\n", header.Name, header.Size)
+						log.Printf("  Found file in tar: %s (size: %d bytes)\n", header.Name, header.Size)
 						if strings.HasSuffix(header.Name, ".md") {
 							mdFileCount++
 							if mdFileCount > 1 {
-								fmt.Printf("  Found multiple .md files, skipping content display\n")
+								log.Printf("  Found multiple .md files, skipping content display\n")
 								break
 							}
 							singleMdFileName = header.Name
@@ -112,36 +111,36 @@ func ScanLayersForModelCarD(layers []types.BlobInfo, src types.ImageSource) {
 					}
 
 					if mdFileCount == 1 {
-						fmt.Printf("  Found single .md file: %s (size: %d bytes)\n", singleMdFileName, len(singleMdContent))
+						log.Printf("  Found single .md file: %s (size: %d bytes)\n", singleMdFileName, len(singleMdContent))
 						lines := bytes.Split(singleMdContent, []byte("\n"))
-						fmt.Printf("  First 10 lines of %s:\n", singleMdFileName)
-						fmt.Printf("  ----------------------------------------\n")
+						log.Printf("  First 10 lines of %s:\n", singleMdFileName)
+						log.Printf("  ----------------------------------------\n")
 						for j, line := range lines {
 							if j >= 10 {
 								break
 							}
-							fmt.Printf("  %d: %s\n", j+1, string(line))
+							log.Printf("  %d: %s\n", j+1, string(line))
 						}
-						fmt.Printf("  ----------------------------------------\n")
+						log.Printf("  ----------------------------------------\n")
 					} else {
-						fmt.Printf("  No .md files found in the blob\n")
+						log.Printf("  No .md files found in the blob\n")
 					}
 				}
 			}
 		}
-		fmt.Println()
+
 	}
 }
 
 func FetchManifestSrcAndLayers(manifestRef string, sys *types.SystemContext) (types.ImageSource, []types.BlobInfo) {
-	fmt.Printf("Parsing reference...\n")
+	log.Printf("Parsing reference...\n")
 	ref, err := docker.ParseReference("//" + manifestRef)
 	if err != nil {
 		log.Fatalf("Failed to parse reference: %v", err)
 	}
 
 	// Create a new image source (later will use to get "the" blob)
-	fmt.Printf("Creating image source...\n")
+	log.Printf("Creating image source...\n")
 	src, err := ref.NewImageSource(context.Background(), sys)
 	if err != nil {
 		log.Fatalf("Failed to create image source: %v", err)
@@ -154,9 +153,8 @@ func FetchManifestSrcAndLayers(manifestRef string, sys *types.SystemContext) (ty
 		log.Fatalf("Failed to get manifest: %v", err)
 	}
 
-	fmt.Printf("Manifest type: %s\n", manifestType)
-	fmt.Printf("Manifest size: %d bytes\n", len(manifest))
-	fmt.Println()
+	log.Printf("Manifest type: %s\n", manifestType)
+	log.Printf("Manifest size: %d bytes\n", len(manifest))
 
 	// Get the image
 	img, err := ref.NewImage(context.Background(), sys)
@@ -166,25 +164,23 @@ func FetchManifestSrcAndLayers(manifestRef string, sys *types.SystemContext) (ty
 	defer img.Close()
 
 	// Get the image configuration
-	fmt.Printf("Getting config blob...\n")
+	log.Printf("Getting config blob...\n")
 	configBlob, err := img.ConfigBlob(context.Background())
 	if err != nil {
 		log.Fatalf("Failed to get config blob: %v", err)
 	}
 
-	fmt.Printf("Config blob size: %d bytes\n", len(configBlob))
-	fmt.Println()
+	log.Printf("Config blob size: %d bytes\n", len(configBlob))
 
 	// Get layer information
-	fmt.Printf("Getting layer infos...\n")
+	log.Printf("Getting layer infos...\n")
 	layers := img.LayerInfos()
-	fmt.Printf("Number of layers: %d\n", len(layers))
-	fmt.Println()
+	log.Printf("Number of layers: %d\n", len(layers))
 
 	// Get layer digests from layer infos
-	fmt.Println("Layer digests:")
+	log.Printf("Layer digests:\n")
 	for i, layer := range layers {
-		fmt.Printf("  Layer %d: %s\n", i+1, layer.Digest)
+		log.Printf("  Layer %d: %s\n", i+1, layer.Digest)
 	}
 	return src, layers
 }
